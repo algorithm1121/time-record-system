d2ce11c167fc83f19950aa97b85ba6bc38362657
Erin Cao <zhencao93@gmail.com>
2016-06-02 10:54:12 +0800

    完成springMVC 简单配置

--- src/main/java/com/learn/controller/HelloWorldController.java
+++ src/main/java/com/learn/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.learn.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Erin on 6/2/16.
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
 
     @RequestMapping(value = "hello")
-    public String hello(){
+    public ModelAndView hello(String name, ModelAndView modelAndView){
 
-        return "hello";
+        modelAndView.addObject("name", name);
+        modelAndView.setViewName("hello");
+        return modelAndView;
     }
 }
--- web/WEB-INF/jsp/hello.jsp
+++ web/WEB-INF/jsp/hello.jsp
@@ -6,11 +6,12 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
-    <title>Hello</title>
+    <title>Hello </title>
 </head>
 <body>
-
+<h1> hi ${name} </h1>
 </body>
 </html>
